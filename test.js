f431616d5c929fbb3cc18363b7159601e022751f
ifad <yusuffadhlih123@gmail.com>
2025-01-31 22:50:04 +0700

    initial commit

---
+++ test.js
@@ -0,0 +1,8 @@
+// const unixTimestampMs = new Date().toUTCString();
+// console.log(unixTimestampMs);
+
+function date(date) {
+  console.log(new Date(date).toUTCString());
+}
+
+date(1738336314902);
